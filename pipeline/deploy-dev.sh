c33e08b09f065dc71ab605f39d940aefdba97fe5
EC2 Default User <ec2-user@ip-172-31-41-158.eu-west-1.compute.internal>
2019-02-21 19:04:24 +0000

    add dev deployment script

---
+++ pipeline/deploy-dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export S3_DEV_BUCKET=awsteam-development-build-assets
+export STACK_NAME=awsteam-samuel-development-stack
+export DevPortalSiteS3BucketName=awsteam-samuel-development-serverless-portal
+
+# Valdidate cloudformation template
+aws cloudformation validate-template --template-body file://template.yaml
+
+# Package cloudformation package
+aws cloudformation package --template template.yaml --s3-bucket "$S3_DEV_BUCKET" --output-template template-export-dev.yaml
+
+# Deploy cloudformation package
+sam deploy --template-file template-export-dev.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides DevPortalSiteS3BucketName=$DevPortalSiteS3BucketName ArtifactsS3BucketName=$S3_DEV_BUCKET
