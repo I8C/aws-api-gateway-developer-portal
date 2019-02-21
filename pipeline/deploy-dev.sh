d778db7d0b6a5b6a2ab877addec62d03fba2780b
Samuel Vandecasteele <samuel.vandecasteele@i8c.be>
2019-02-21 19:20:01 +0000

    update deploy script

--- pipeline/deploy-dev.sh
+++ pipeline/deploy-dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export S3_DEV_BUCKET=awsteam-development-build-assets
+export S3_PORTAL_ARTIFACTS=awsteam-samuel-development-serverless-portal-artifacts-bucket
 export STACK_NAME=awsteam-samuel-development-stack
 export DevPortalSiteS3BucketName=awsteam-samuel-development-serverless-portal
 
@@ -10,4 +11,4 @@ aws cloudformation validate-template --template-body file://template.yaml
 aws cloudformation package --template template.yaml --s3-bucket "$S3_DEV_BUCKET" --output-template template-export-dev.yaml
 
 # Deploy cloudformation package
-sam deploy --template-file template-export-dev.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides DevPortalSiteS3BucketName=$DevPortalSiteS3BucketName ArtifactsS3BucketName=$S3_DEV_BUCKET
+sam deploy --template-file template-export-dev.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --parameter-overrides DevPortalSiteS3BucketName=$DevPortalSiteS3BucketName ArtifactsS3BucketName=$S3_PORTAL_ARTIFACTS
