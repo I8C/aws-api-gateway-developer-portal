eb313dd680a8152ab437e1ea8a2448e8567d9f59
Samuel Vandecasteele <samuel.vandecasteele@i8c.be>
2019-02-25 15:16:39 +0000

    add cicd infra

---
+++ pipeline/deploy-cicd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export STACK_NAME=awsteam-serverless-portal-cicd-environment
+
+aws cloudformation validate-template --template-body file://cfn-cicd-template.yaml
+
+aws cloudformation create-stack \
+--template-body file://cfn-cicd-template.yaml \
+--stack-name $STACK_NAME \
+--parameters file://params.json \
+--capabilities CAPABILITY_NAMED_IAM
